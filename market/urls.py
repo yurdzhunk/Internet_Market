8b1966843b07e34e609ead498b650603e8c7bd7d
Valeryi Dzhunkovskyi <dzhunkovskyiv@gmail.com>
2016-03-18 15:25:09 +0200

    [DELITION]

--- market/urls.py
+++
@@ -1,5 +0,0 @@
-from django.conf.urls import include, url
-
-urlpatterns = [
-    url(r'^', 'market.views.start_page'),
-]
--- market/views.py
+++
@@ -1,6 +0,0 @@
-from django.shortcuts import render
-
-from django.shortcuts import render_to_response, redirect
-
-def start_page(request):
-    return render_to_response('start_page.html')
