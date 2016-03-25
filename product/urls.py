26dc12dc7956c4ed77a839cb70c3e06f79383540
Valeryi Dzhunkovskyi <dzhunkovskyiv@gmail.com>
2016-03-25 20:54:05 +0200

    [market_sectors_added]

--- product/urls.py
+++ product/urls.py
@@ -9,6 +9,9 @@ urlpatterns = [
     url(r'^product/addlike/(?P<path_argument>\d+)/(?P<product_id>\d+)/(?P<comment_page_number>\d+)/$', 'product.views.addlike'),
     url(r'^product/addcomment/(?P<product_id>\d+)/$', 'product.views.addcomment'),
     url(r'^market/$', 'product.views.market'),
+    url(r'^shop/notebook/$', 'product.views.notebook'),
+    url(r'^shop/smartphones/$', 'product.views.smartphone'),
+    url(r'^shop/tv/$', 'product.views.tv'),
     url(r'^', 'product.views.startpage'),
 
 ]
